56e8fb02171c95f03070e3f81ada699ec735cdd1
Nolen Johnson <johnsonnolen@gmail.com>
2021-11-23 15:05:41 -0500

    Set camera extension package to Google package
    Change-Id: I519ebf8a1e2a9d06561471206c91c01b352ebd47

--- calyx_common.mk
+++ calyx_common.mk
@@ -7,6 +7,11 @@
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/gs201/overlay-calyx
 
+# Camera
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
+    ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
+
 # Lineage Health
 include hardware/google/pixel/lineage_health/device.mk
 
